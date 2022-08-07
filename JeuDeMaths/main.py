e64d7604cebacf53ad5a204540668330b10107a1
Salah KHALFI <skhalfi19@gmail.com>
2022-08-07 15:44:02 +0200

    Avec deux opérendes aléatoires

--- JeuDeMaths/main.py
+++ JeuDeMaths/main.py
@@ -1,17 +1,24 @@
 import random
-import re
 
 NOMBRE_MIN = 1
 NOMBRE_MAX = 10
 
 # poser les questions à l'utilisateur
 def poser_questions():
-    resultat = int(input(f"Entrez votre réponse de l'opération : 5 + 2 "))
-    if resultat == 5 + 2:
+
+    nombre1 = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    nombre2 = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+
+    # Résultat de l'addition
+    addition = nombre1 + nombre2
+
+    # Demander le résultat de l'addition à l'utilisateur
+    resultat = int(input(f"Votre réponse de l'addition {nombre1} + {nombre2} : "))
+    
+    if resultat == addition:
         return f"votre réponse '{resultat}' est correct"
-    else:
-        return f"votre réponse '{resultat} ' est incorrect"
+    else: 
+        return f"votre réponse '{resultat}' est incorrect"
 
-print(f"résultat : {poser_questions()}")
+print(f" ^^^: {poser_questions()}")
         
-
